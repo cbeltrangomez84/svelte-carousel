62d7e020aefcaea337d8c330b2d8c8b1a8fdabf3
Carlos Beltrán <cbeltrangomez@gmail.com>
2024-06-07 08:16:13 -0500

    Path resolution

--- src/utils/page.js
+++ src/utils/page.js
@@ -1,67 +1,45 @@
-import {
-  getValueInRange,
-} from './math'
+import { getValueInRange } from "./math.js"
 
 // getCurrentPageIndexByCurrentParticleIndex
 
-export function _getCurrentPageIndexByCurrentParticleIndexInfinite({
-  currentParticleIndex,
-  particlesCount,
-  clonesCountHead,
-  clonesCountTotal,
-  particlesToScroll,
-}) {
+export function _getCurrentPageIndexByCurrentParticleIndexInfinite({ currentParticleIndex, particlesCount, clonesCountHead, clonesCountTotal, particlesToScroll }) {
   if (currentParticleIndex === particlesCount - clonesCountHead) return 0
-  if (currentParticleIndex === 0) return _getPagesCountByParticlesCountInfinite({
-    particlesCountWithoutClones: particlesCount - clonesCountTotal,
-    particlesToScroll,
-  }) - 1
+  if (currentParticleIndex === 0)
+    return (
+      _getPagesCountByParticlesCountInfinite({
+        particlesCountWithoutClones: particlesCount - clonesCountTotal,
+        particlesToScroll,
+      }) - 1
+    )
   return Math.floor((currentParticleIndex - clonesCountHead) / particlesToScroll)
 }
 
-export function _getCurrentPageIndexByCurrentParticleIndexLimited({
-  currentParticleIndex,
-  particlesToScroll,
-}) {
+export function _getCurrentPageIndexByCurrentParticleIndexLimited({ currentParticleIndex, particlesToScroll }) {
   return Math.ceil(currentParticleIndex / particlesToScroll)
 }
 
-export function getCurrentPageIndexByCurrentParticleIndex({
-  currentParticleIndex,
-  particlesCount,
-  clonesCountHead,
-  clonesCountTotal,
-  infinite,
-  particlesToScroll,
-}) {
+export function getCurrentPageIndexByCurrentParticleIndex({ currentParticleIndex, particlesCount, clonesCountHead, clonesCountTotal, infinite, particlesToScroll }) {
   return infinite
     ? _getCurrentPageIndexByCurrentParticleIndexInfinite({
-      currentParticleIndex,
-      particlesCount,
-      clonesCountHead,
-      clonesCountTotal,
-      particlesToScroll,
-    })
+        currentParticleIndex,
+        particlesCount,
+        clonesCountHead,
+        clonesCountTotal,
+        particlesToScroll,
+      })
     : _getCurrentPageIndexByCurrentParticleIndexLimited({
-      currentParticleIndex,
-      particlesToScroll,
-    })
+        currentParticleIndex,
+        particlesToScroll,
+      })
 }
 
 // getPagesCountByParticlesCount
 
-export function _getPagesCountByParticlesCountInfinite({
-  particlesCountWithoutClones,
-  particlesToScroll,
-}) {
+export function _getPagesCountByParticlesCountInfinite({ particlesCountWithoutClones, particlesToScroll }) {
   return Math.ceil(particlesCountWithoutClones / particlesToScroll)
 }
 
-export function _getPagesCountByParticlesCountLimited({
-  particlesCountWithoutClones,
-  particlesToScroll,
-  particlesToShow,
-}) {
+export function _getPagesCountByParticlesCountLimited({ particlesCountWithoutClones, particlesToScroll, particlesToShow }) {
   const partialPageSize = getPartialPageSize({
     particlesCountWithoutClones,
     particlesToScroll,
@@ -70,97 +48,58 @@ export function _getPagesCountByParticlesCountLimited({
   return Math.ceil(particlesCountWithoutClones / particlesToScroll) - partialPageSize
 }
 
-export function getPagesCountByParticlesCount({
-  infinite,
-  particlesCountWithoutClones,
-  particlesToScroll,
-  particlesToShow,
-}) {
+export function getPagesCountByParticlesCount({ infinite, particlesCountWithoutClones, particlesToScroll, particlesToShow }) {
   return infinite
     ? _getPagesCountByParticlesCountInfinite({
-      particlesCountWithoutClones,
-      particlesToScroll,
-    })
+        particlesCountWithoutClones,
+        particlesToScroll,
+      })
     : _getPagesCountByParticlesCountLimited({
-      particlesCountWithoutClones,
-      particlesToScroll,
-      particlesToShow,
-    })
+        particlesCountWithoutClones,
+        particlesToScroll,
+        particlesToShow,
+      })
 }
 
 // getParticleIndexByPageIndex
 
-export function _getParticleIndexByPageIndexInfinite({
-  pageIndex,
-  clonesCountHead,
-  clonesCountTail,
-  particlesToScroll,
-  particlesCount,
-}) {
-  return getValueInRange(
-    0,
-    Math.min(clonesCountHead + pageIndex * particlesToScroll, particlesCount - clonesCountTail),
-    particlesCount - 1
-  )
+export function _getParticleIndexByPageIndexInfinite({ pageIndex, clonesCountHead, clonesCountTail, particlesToScroll, particlesCount }) {
+  return getValueInRange(0, Math.min(clonesCountHead + pageIndex * particlesToScroll, particlesCount - clonesCountTail), particlesCount - 1)
 }
 
-export function _getParticleIndexByPageIndexLimited({
-  pageIndex,
-  particlesToScroll,
-  particlesCount,
-  particlesToShow,
-}) {
-  return getValueInRange(
-    0,
-    Math.min(pageIndex * particlesToScroll, particlesCount - particlesToShow),
-    particlesCount - 1
-  ) 
+export function _getParticleIndexByPageIndexLimited({ pageIndex, particlesToScroll, particlesCount, particlesToShow }) {
+  return getValueInRange(0, Math.min(pageIndex * particlesToScroll, particlesCount - particlesToShow), particlesCount - 1)
 }
 
-export function getParticleIndexByPageIndex({
-  infinite,
-  pageIndex,
-  clonesCountHead,
-  clonesCountTail,
-  particlesToScroll,
-  particlesCount,
-  particlesToShow,
-}) {
+export function getParticleIndexByPageIndex({ infinite, pageIndex, clonesCountHead, clonesCountTail, particlesToScroll, particlesCount, particlesToShow }) {
   return infinite
     ? _getParticleIndexByPageIndexInfinite({
-      pageIndex,
-      clonesCountHead,
-      clonesCountTail,
-      particlesToScroll,
-      particlesCount,
-    })
+        pageIndex,
+        clonesCountHead,
+        clonesCountTail,
+        particlesToScroll,
+        particlesCount,
+      })
     : _getParticleIndexByPageIndexLimited({
-      pageIndex,
-      particlesToScroll,
-      particlesCount,
-      particlesToShow,
-    })
+        pageIndex,
+        particlesToScroll,
+        particlesCount,
+        particlesToShow,
+      })
 }
 
-export function applyParticleSizes({
-  particlesContainerChildren,
-  particleWidth,
-}) {
-  for (let particleIndex=0; particleIndex<particlesContainerChildren.length; particleIndex++) {
+export function applyParticleSizes({ particlesContainerChildren, particleWidth }) {
+  for (let particleIndex = 0; particleIndex < particlesContainerChildren.length; particleIndex++) {
     particlesContainerChildren[particleIndex].style.minWidth = `${particleWidth}px`
     particlesContainerChildren[particleIndex].style.maxWidth = `${particleWidth}px`
   }
 }
 
-export function getPartialPageSize({
-  particlesToScroll,
-  particlesToShow,
-  particlesCountWithoutClones, 
-}) {
+export function getPartialPageSize({ particlesToScroll, particlesToShow, particlesCountWithoutClones }) {
   const overlap = particlesToScroll - particlesToShow
   let particlesCount = particlesToShow
 
-  while(true) {
+  while (true) {
     const diff = particlesCountWithoutClones - particlesCount - overlap
     if (diff < particlesToShow) {
       return Math.max(diff, 0) // show: 2; scroll: 3, n: 5 => -1
@@ -170,9 +109,9 @@ export function getPartialPageSize({
 }
 
 export function createResizeObserver(onResize) {
-  return new ResizeObserver(entries => {
+  return new ResizeObserver((entries) => {
     onResize({
       width: entries[0].contentRect.width,
     })
-  });
+  })
 }
